7a949a074fbb3b3248146b19a8d0abe6ef672334
sam <sd@sams-iMac.local>
2021-08-20 17:00:51 -0700

    added randomness to the texture positions when the velocity is added so the texture positions dont group up to form lines

--- src/shaders/vertex.js
+++ src/shaders/vertex.js
@@ -14,18 +14,18 @@ void main()
 
     vec3 pos = texture(positionTexture, vUv).xyz;
     vec3 vel = texture(velocityTexture, vUv).xyz;
-
+/*
     if(pos.x > 0.01)
     {
       //gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
       pos.xy = position.xy;
       pos.z = 0.0;
     }
-
+*/
     vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
 
     //gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(pos, 1.0);
     gl_Position = projectionMatrix * mvPosition;
-    gl_PointSize = 2.0;
+    gl_PointSize = 3.0;
 }
 `
